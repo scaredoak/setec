953a4ad62150c74d49a45e8146ef3185a42d0fc8
Eudes <eudes.scar@gmail.com>
2026-02-17 00:05:18 -0300

    feat: add `ProductRepository` with native query

---
+++ src/main/java/com/setec/crud/repository/ProductRepository.java
@@ -0,0 +1,19 @@
+package com.setec.crud.repository;
+
+import com.setec.crud.domain.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    @Query(value = "SELECT * FROM products", nativeQuery = true)
+    List<Product> findAll();
+
+    @Query(value = "SELECT * FROM products WHERE id = :id", nativeQuery = true)
+    Optional<Product> findById(Long id);
+
+    @Query(value = "SELECT * FROM products WHERE description = :description", nativeQuery = true)
+    Optional<Product> findByDescription(String description);
+}
