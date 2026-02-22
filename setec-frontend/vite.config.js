16c0193cbdc7f1098ad6f09fc8b38680f2055416
Eudes <eudes.scar@gmail.com>
2026-02-22 19:37:14 -0300

    feat: add scaffolding for ReactJS project

---
+++ setec-frontend/vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [react()],
+    server: {
+      port: env.REACT_APP_PORT ? env.REACT_APP_PORT : 5173,
+      host: '0.0.0.0'
+    },
+  }
+})
