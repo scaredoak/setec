c3a7692670ba340a2814aef6ff148943dfe80fb2
Eudes <eudes.scar@gmail.com>
2026-02-25 19:08:03 -0300

    feat(front): add `BrowserRouter` and navigation placeholders

--- setec-frontend/src/App.jsx
+++ setec-frontend/src/App.jsx
@@ -1,9 +1,31 @@
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom"
 import Clients from "./pages/Clients"
+import Home from "./pages/Home"
+
+function About() {
+  return <h1>About page!</h1>
+}
 
 function App() {
   return (
     <>
-      <Clients/>
+      <BrowserRouter>
+        <nav>
+          <Link to="/">Início</Link> |{" "}
+          <Link to="/clientes">Clientes</Link> |{" "}
+          <Link to="/produtos">Produtos</Link> |{" "}
+          <Link to="/pedidos">Pedidos</Link> |{" "}
+          <Link to="/about">About</Link>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/clientes" element={<Clients />} />
+          <Route path="/produtos" element={<Clients />} />
+          <Route path="/pedidos" element={<Clients />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
