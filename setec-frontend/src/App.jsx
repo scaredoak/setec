9e70155358cbb085503cacd2c7966c0e2caa3984
Eudes <eudes.scar@gmail.com>
2026-02-25 18:22:07 -0300

    refactor(front): move client component to separate directory

--- setec-frontend/src/App.jsx
+++ setec-frontend/src/App.jsx
@@ -1,78 +1,9 @@
-import { useEffect, useState } from "react"
-import clientService from "./api/clientService"
+import Clients from "./pages/Clients"
 
 function App() {
-  const [costumers, setCostumers] = useState([])
-  const [costumerForm, setCostumerForm] = useState({
-    name: "",
-    email: ""
-  })
-
-  useEffect(() => {
-    clientService.getAll()
-      .then(res => {
-        setCostumers(res.data)
-      })
-      .catch(e => {
-        console.error("Erro: ", e)
-      })
-  }, [])
-
-  function handleChange(e) {
-    const { name, value } = e.target
-
-    setCostumerForm(previous => ({ ...previous, [name]: value }))
-  }
-
-  async function handleSubmit(e) {
-    e.preventDefault()
-
-    try {
-      await clientService.create(costumerForm)
-      const newCostumers = await clientService.getAll()
-      setCostumers(newCostumers.data)
-    } catch (e) {
-      console.error(e)
-    }
-  }
-
   return (
     <>
-      <h1>Clientes</h1>
-      <div>
-        <form onSubmit={handleSubmit}>
-          <label>Nome: </label><input name="name" type="text" onChange={handleChange} required /><br />
-
-          <label>Email: </label><input name="email" type="email" onChange={handleChange} required />
-          <br />
-
-          <button type="submit">Cadastrar cliente</button>
-        </form>
-
-        <br />
-
-        <table id="costumers">
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>NOME</th>
-              <th>EMAIL</th>
-            </tr>
-          </thead>
-
-          <tbody>
-            {costumers.map(client => {
-              return (
-                <tr key={crypto.randomUUID()}>
-                  <td>{client.id}</td>
-                  <td>{client.name}</td>
-                  <td>{client.email}</td>
-                </tr>
-              )
-            })}
-          </tbody>
-        </table>
-      </div>
+      <Clients/>
     </>
   )
 }
