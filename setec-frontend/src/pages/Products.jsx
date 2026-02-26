805d10d469cfbb266a5f423f871b094c145a527d
Eudes <eudes.scar@gmail.com>
2026-02-26 07:48:18 -0300

    fix(front): typo

--- setec-frontend/src/pages/Products.jsx
+++ setec-frontend/src/pages/Products.jsx
@@ -59,7 +59,7 @@ export default function Products() {
           <thead>
             <tr>
               <th>ID</th>
-              <th>DESCRIÇão</th>
+              <th>DESCRIÇÃO</th>
               <th>PREÇO</th>
               <th>QUANTIDADE EM ESTOQUE</th>
             </tr>
